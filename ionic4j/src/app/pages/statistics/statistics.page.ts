b620f7e71cbeb84a33c31b524e09f8961d769012
Blancamauri <Blamaurob@alum.us.es>
2022-07-16 18:39:00 +0200

    vistas estadisticas creadas

--- ionic4j/src/app/pages/statistics/statistics.page.ts
+++ ionic4j/src/app/pages/statistics/statistics.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-statistics',
@@ -6,7 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.page.scss'],
 })
 export class StatisticsPage implements OnInit {
-  constructor() {}
+  private selectedEvento: string;
+  private selectedEquipo: string;
+  constructor(private pickerController: PickerController) {}
 
   ngOnInit() {}
+
+  async eventosButton() {
+    const picker = await this.pickerController.create({
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: selected => {
+            this.selectedEvento = selected.evento.value;
+            this.selectedEquipo = selected.equipo.value;
+          },
+        },
+      ],
+      columns: [
+        {
+          name: 'evento',
+          options: [
+            { text: 'Amarilla', value: 'amarilla' },
+            { text: 'Roja', value: 'roja' },
+            { text: 'Gol', value: 'gol' },
+            { text: 'Falta', value: 'falta' },
+            { text: 'Tiro', value: 'tiro' },
+            { text: 'Corner', value: 'corner' },
+          ],
+        },
+        {
+          name: 'equipo',
+          options: [
+            { text: 'Equipo local', value: 'local' },
+            { text: 'Equipo visitante', value: 'visitante' },
+          ],
+        },
+      ],
+    });
+    await picker.present();
+  }
 }
